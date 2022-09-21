1c37e14d626e71a9bf10f8b33d8cf0cb7d2f39a1
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-21 23:49:43 +0530

    Precision in C++

---
+++ Precision.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(void)
+{
+	cout.precision(3);
+	cout<<sqrt(2)<<endl;
+	cout<<3.14159<<endl;
+	cout<<34.56234<<endl;
+	return 0;
+}
