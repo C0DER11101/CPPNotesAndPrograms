b6a8f82a10dda07b4d7c950f2a30a3b98fbc1002
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-22 11:00:44 +0530

    Understanding width() in C++

---
+++ test5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout.width(5);
+	cout<<12345<<endl;
+
+	cout.width(10);
+	cout<<12345<<endl;
+
+	cout.width(7);
+	cout<<12345<<endl;
+
+	return 0;
+}
