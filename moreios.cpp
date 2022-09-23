19c4cc195d511b95787eef1ce1fc57f05c26d0dd
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-23 10:35:23 +0530

    ios::showpoint

---
+++ moreios.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	//cout.setf(ios::showpoint);
+	cout.precision(3);
+	cout<<27.5<<endl;
+	cout<<25.0<<endl;
+	cout<<23.45678<<endl;
+	return 0;
+}
