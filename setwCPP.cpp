4f002421b28358219a65545268d8c79ca6a7c46f
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-23 18:37:46 +0530

    setw() in C++

---
+++ setwCPP.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(void)
+{
+	// setw() works exactly like width()
+	cout<<setw(5)<<12<<endl;
+	cout<<setw(10)<<"Priyanuj"<<endl;
+
+
+	return 0;
+}
