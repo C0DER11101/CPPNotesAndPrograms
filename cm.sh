26ea855d79f3fb166919facf7582476d9857d0d5
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-25 20:31:35 +0530

    Compile C++ programs(also the ones containing math.h header file

--- cm.sh
+++ cm.sh
@@ -4,7 +4,18 @@ if [ -e $1 ]
 then
 	if [ -f $1 ]
 	then
-		g++ $1 -o $2
+		while read p
+		do
+			if [ p == "math.h" ]
+			then
+				g++ $1 -o $2 -lm
+				break;
+
+			else
+				g++ $1 -o $2
+				break;
+			fi
+		done < $1
 	else
 		echo "$1 is not a file!"
 
