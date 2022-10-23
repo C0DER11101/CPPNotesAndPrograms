a31c3a775558814ef76b131c283295fe864a6d2e
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-23 23:48:26 +0530

    Counter program of test5.cpp

---
+++ tests/test5Counter.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+
+using namespace std;
+
+
+class sample
+{
+	int _var;
+
+	public:
+
+	sample(int val): _var(val){}
+
+	void power(int raise=2) const; // const member function
+	
+};
+
+void sample::power(int raise) const
+{
+	int product=1;
+	cout<<_var<<" ^ "<<raise<<" = ";
+
+	for(int i=0; i<raise; i++)
+		product*=_var;
+
+	cout<<product<<"\n";
+}
+
+int main(void)
+{
+
+	sample s(2);
+
+	s.power(3);
+
+	return 0;
+}
