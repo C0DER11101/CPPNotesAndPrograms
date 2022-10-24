959aa50e2a71c211dec430538609347484475c18
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-24 07:01:41 +0530

    Negating an integer

---
+++ tests/test6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+
+using namespace std;
+
+int main(void)
+{
+	int a;
+	cout<<"enter a: ";
+	cin>>a;
+
+	cout<<"a = "<<a<<"\n";
+
+	a=-a;
+
+	cout<<"a = "<<a<<"\n\n";
+	return 0;
+}
