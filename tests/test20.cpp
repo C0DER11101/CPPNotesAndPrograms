a1851cd4f0c161cc6ae2a2ac89297c964ab569c3
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-27 14:08:54 +0530

    Strange Error Gone

---
+++ tests/test20.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdlib.h>
+#include<iomanip>
+#include<conio.h>
+
+using namespace std;
+
+template<class T>
+void Swap(T&, T&);
+
+int main(void)
+{
+	int a, b;
+
+	a=10, b=20;
+
+	cout<<"a"<<setw(20)<<"b"<<"\n";
+	cout<<a<<setw(20)<<b<<"\n\n";
+
+	Swap(a, b);
+	cout<<"After swapping:\n";
+	cout<<"a"<<setw(20)<<"b"<<"\n";
+	cout<<a<<setw(20)<<b<<"\n\n";
+	return 0;
+}
+
+template<class T>
+void Swap(T&a, T&b)
+{
+	T temp;
+
+	temp=a;
+	a=b;
+	b=temp;
+}
