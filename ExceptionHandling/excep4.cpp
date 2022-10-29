0aad8fc13f211aa65fb8febdd4a0a44abc7e42df
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-29 06:58:23 +0530

    Catching all exceptionsgit add excep4.cpp

---
+++ ExceptionHandling/excep4.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+template<class T>
+void test(T);
+
+int main(void)
+{
+	int a=10;
+	float b=13.56;
+	double d=67.8;
+	char c='a';
+	
+	cout<<"\nFrom main:\n";
+	test(a);
+
+	cout<<"\nBack to main:\n";
+	test(b);
+
+	cout<<"\nBack to main:\n";
+	test(d);
+
+	cout<<"\nBack to main:\n";
+	test(c);
+	
+	return 0;
+}
+
+template<class T>
+void test(T a)
+{
+	try
+	{
+		throw a;
+	}
+
+	catch(...) // catch all the exceptions!!
+	{
+		cout<<"\nCaught "<<a<<"\n\n";
+	}
+}
