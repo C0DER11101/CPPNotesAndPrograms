627f2877478819c7a0171984749db3a993994b36
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-30 06:57:36 +0530

    Throwing a pointer-to-integer; Throwing a pointer-to-pointer-to-integer

---
+++ tests/test25.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void)
+{
+	int *a=new int[5];
+
+	try
+	{
+		throw a;
+	}
+
+	catch(int*c)
+	{
+		cout<<"\nCaught a pointer to integer!!\n\n";
+	}
+
+	catch(...)
+	{
+		cout<<"\nSomething else!!\n\n";
+	}
+
+	int **b;
+
+	try
+	{
+		throw b;
+	}
+
+	catch(int*a)
+	{
+		cout<<"\nCaught a pointer to integer!!\n\n";
+	}
+
+	catch(int**c)
+	{
+		cout<<"\nCaught a pointer to pointer to integer!!\n\n";
+	}
+
+	catch(...)
+	{
+		cout<<"\nSomething else!!\n\n";
+	}
+
+	delete a;
+
+	return 0;
+}
