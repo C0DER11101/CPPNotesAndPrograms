20e52a80b323d685081158c4c106708d0c488a93
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-07 14:28:08 +0530

    Re-throwing an exception

---
+++ ExceptionHandling/Exercises/ex2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+template<class T>
+void add(T a, T b)
+{
+	try
+	{
+		if(a<0 || b<0)
+			throw (a+b);
+	}
+
+	catch(int result)
+	{
+		cout<<"\n[Calculating....]\n";
+		system("sleep 0.2");
+		throw;
+	}
+
+	cout<<"\n"<<a<<" + "<<b<<" = "<<a+b<<"\n";
+}
+
+int main(void)
+{
+	int i1, i2;
+
+	cout<<"enter two positive integers: ";
+	cin>>i1>>i2;
+
+	try
+	{
+		add(i1, i2);
+	}
+
+	catch(int)
+	{
+		cout<<"\nNegative numbers are not allowed!!\n";
+	}
+
+	return 0;
+}
