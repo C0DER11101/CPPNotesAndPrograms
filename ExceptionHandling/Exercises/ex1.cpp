698492ed49976fc16e21c6aebb05ce7794a1b128
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-07 13:57:55 +0530

    Exercise1

--- ExceptionHandling/Exercises/ex1.cpp
+++ ExceptionHandling/Exercises/ex1.cpp
@@ -2,7 +2,56 @@
 
 using namespace std;
 
+template<class T>
+void test(T var) throw(char, int)
+{
+	cout<<"\nInside test()\n";
+	try
+	{
+		if(var==4.5)
+			throw var;
+	}
+
+	catch(double v)
+	{
+		cout<<"\nCaught a double!\n";
+	}
+
+	if(var=='x')
+		throw var;
+
+	if(var==1)
+		throw var;
+}
+
 int main(void)
 {
+	cout<<"\nInside main()\n";
+	try
+	{
+		test('x');
+		test(1);
+		test(4.5);
+	}
+
+	catch(char)
+	{
+		cout<<"\nBack in main()\n";
+		cout<<"\nCaught a character!!\n";
+	}
+
+	catch(int)
+	{
+		cout<<"\nBack in main()\n";
+		cout<<"\nCaught an integer!!\n";
+	}
+
+	catch(double)
+	{
+		cout<<"\nBack in main()\n";
+		cout<<"\nCaught an double!!\n";
+	}
+
+
 	return 0;
 }
