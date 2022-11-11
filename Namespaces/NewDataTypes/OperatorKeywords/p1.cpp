878aecae8e68f338764ccb2ead6841a206230769
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-11 12:14:06 +0530

    Using operator keywords

---
+++ Namespaces/NewDataTypes/OperatorKeywords/p1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cout<<"enter two values: ";
+	cin>>a>>b;
+
+	if(a == b and b not_eq 0)
+		cout<<"\nGood input!!\n";
+	else if(a not_eq b and b not_eq 0)
+		cout<<"\nFair input!!\n";
+	else
+		cout<<"\nBad input!!\n";
+	return 0;
+}
